70d755cab15004559b1c0e60f3dd884d3040c9f9
Rohit77-eng <82858877+Rohit77-eng@users.noreply.github.com>
2023-06-21 15:04:58 +0530

    Delete application.py

--- application.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd 
-import numpy as np 
-import pickle
-
-model = pickle.load(open('dt_classifier.pkl', 'rb'))
-# Set the title of the app
-st.title('Car Selling Price Prediction')
-
